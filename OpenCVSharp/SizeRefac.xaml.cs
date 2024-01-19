55b3dce9ff8df78fd9666f2072b891cd0c08e636
AhnYeonghoo <dksdudgn00@naver.com>
2024-01-19 16:42:53 +0900

    d

---
+++ OpenCVSharp/SizeRefac.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System.Windows.Threading;
+
+namespace OpenCVSharp
+{
+    /// <summary>
+    /// Interaction logic for SizeRefac.xaml
+    /// </summary>
+    public partial class SizeRefac : System.Windows.Window
+    {
+        VideoCapture cam;
+        Mat frame;
+        DispatcherTimer timer;
+        bool isInitCam, isInitTimer;
+
+        public SizeRefac()
+        {
+            //Mat src = new Mat("PaperAirplane.webp");
+            //Mat dst = new Mat();
+            //image1.Source = OpenCvSharp.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(src);
+            //Cv2.Resize(src, dst, new OpenCvSharp.Size(500, 250));
+
+            //image1.Source = OpenCvSharp.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(src);
+            //image2.Source = OpenCvSharp.WpfExtensions.WriteableBitmapConverter.ToWriteableBitmap(dst);
+            //Cv2.WaitKey(0);
+            InitializeComponent();
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            isInitCam = InitCamera();
+            isInitTimer = InitTimer(0.01);
+            if (isInitCam && isInitTimer) timer.Start();
+        }
+
+        private bool InitTimer(double intervalMs)
+        {
+            try
+            {
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(intervalMs);
+                timer.Tick += new EventHandler(TimerTick);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool InitCamera()
+        {
+            try
+            {
+                cam = new VideoCapture(0);
+                cam.FrameHeight = (int)Cam_1.Height;
+                cam.FrameWidth = (int)Cam_1.Width;
+                frame = new Mat();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            cam.Read(frame);
+            Cam_1.Source = OpenCvSharp
+                            .WpfExtensions
+                            .WriteableBitmapConverter
+                            .ToWriteableBitmap(frame);
+        }
+
+      
+            
+     
+    }
+}
---
+++ OpenCVSharp/Window1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+
+using OpenCvSharp;
+
+namespace OpenCVSharp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : System.Windows.Window
+    {
+        private VideoCapture capVideo;
+        private VideoCapture capCamera;
+
+        int sleepTime;
+
+        Mat matImage = new Mat();
+
+        public Window1()
+        {
+            InitializeComponent();
+            InitializeVideo();
+            InitializeCamera();
+        }
+
+        private void InitializeCamera()
+        {
+            capCamera = new VideoCapture(0);
+        }
+
+        private void InitializeVideo()
+        {
+            string filePath = $"Empty";
+            capVideo = new VideoCapture(filePath);
+            sleepTime = (int)Math.Round(1000 / capVideo.Fps);
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }    
+
+        
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (capVideo.IsOpened())
+            {
+                capVideo.Dispose();
+            }
+        }
+
+        
+    }
+}
