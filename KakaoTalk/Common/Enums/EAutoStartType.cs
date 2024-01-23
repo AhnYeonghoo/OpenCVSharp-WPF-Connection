b7bc94b907cb67066c943cca36edacc82bb84681
AhnYeonghoo <dksdudgn00@naver.com>
2024-01-23 16:55:17 +0900

    Write Helper

---
+++ KakaoTalk/Common/Enums/EAutoStartType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Converters;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Enums
+{
+    [TypeConverter(typeof(EnumDescriptiponConverter<EAutoStartType>))]
+    public enum EAutoStartType
+    {
+        [Description("잠금모드")]
+        LockType,
+        [Description("잠금모드 해제")]
+        UnLockType
+    }
+}
---
+++ KakaoTalk/View/TemplateSelector/ChatTypeStyleSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using Common.Enums;
+
+namespace Views.TemplateSelector
+{ 
+    public class ChatTypeStyleSelector : DataTemplateSelector
+    {
+        public ChatTypeStyleSelector() { }
+        public DataTemplate NormalTemplate { get; set; }
+        public DateTemplate PhotoTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return NormalTemplate;
+        }
+    }
+}
