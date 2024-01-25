ddafaef5e32de4f9c474f0dc4be3ad7821cbca23
AhnYeonghoo <dksdudgn00@naver.com>
2024-01-25 17:25:31 +0900

    Add Enums

---
+++ KakaoTalk/Common/Enums/EChattingType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Common.Converters;
+
+
+namespace Common.Enums
+{
+    [TypeConverter(typeof(EnumDescriptionConverter<EChattingRoomType>))]
+    public enum EChattingRoomType
+    {
+        [Description("1:1채팅")]
+        PersonalChat,
+
+        [Description("그룹")]
+        GroupChat,
+
+        [Description("플러스 친구 채팅")]
+        PlusChat,
+
+        [Description("오픈 채팅")]
+        OpenChat,
+    }
+
+    public enum EChattingMsgType
+    {
+        Normal,
+        Reply,
+        Notice,
+        Photo,
+    }
+
+    public enum EChattingSpeechType
+    {
+        Mine,
+        Opponent,
+    }
+}
---
+++ KakaoTalk/Common/Enums/EFriendUserType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Converters;
+using System.ComponentModel;
+
+namespace Common.Enums
+{
+    [TypeConverter(typeof(EnumDescriptionConverter<EFriendUserType>))]
+    public enum EFriendUserType
+    {
+        [Description("")]
+        None,
+
+        [Description("친구")]
+        Friend,
+
+        [Description("업데이트한 친구")]
+        UpdateFriend,
+
+        [Description("플러스")]
+        PlusFriend,
+
+        [Description("채널")]
+        Channel,
+    }
+}
