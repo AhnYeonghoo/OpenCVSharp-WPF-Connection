273a42032cac663a74d829ab3094e062383df5c0
AhnYeonghoo <dksdudgn00@naver.com>
2024-01-28 17:18:37 +0900

    D

---
+++ KakaoTalk/Common/Controls/ChattingListControl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+
+
+namespace Common.Controls
+{
+    public class ChattingListControl : ListBox
+    {
+        public ChattingListControl()
+        {
+            this.DefaultStyleKey = typeof(ChattingListControl);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+    [TemplatePart(Name = ChatInfoPartName, Type = typeof(Grid))]
+    public class ChattingListItemControl : Control
+    {
+        private const string ChatInfoPartName = "PART_ChatInfo";
+        private Grid? _chatInfoPart;
+        public ChattingListItemControl()
+        {
+            this.DefaultStyleKey = typeof(ChattingListItemControl);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _chatInfoPart = GetTemplateChild(ChatInfoPartName) as Grid;
+        }
+
+        private void ChatInfoPart_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
+            {
+                if (ChatCommand is not null)
+                    ChatCommand.Execute(ChatCommandParameter);
+            }
+        }
+
+        private static readonly DependencyProperty ChatCommandProperty =
+            DependencyProperty.Register(
+                "ChatCommand",
+                typeof(ICommand),
+                typeof(ChattingListItemControl),
+                new UIPropertyMetadata(null));
+
+        public ICommand ChatCommand
+        {
+            set 
+            {
+                SetValue(ChatCommandProperty, value);
+            }
+
+            get
+            {
+                return (ICommand)GetValue(ChatCommandProperty);
+            }
+        }
+
+        public static readonly DependencyProperty ChatCommandParameterProperty =
+            DependencyProperty.Register(
+                "ChatCommandParameter",
+                typeof(object),
+                typeof(ChattingListItemControl),
+                new UIPropertyMetadata(null));
+
+        public Object? ChatCommandParameter
+        {
+            get
+            {
+                return (Object?)GetValue(ChatCommandParameterProperty);
+            }
+            set
+            {
+                SetValue(ChatCommandParameterProperty, value);
+            }
+        }
+    }
+}
---
+++ KakaoTalk/Common/Controls/ChattingMessageControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace Common.Controls
+{
+    [TemplatePart(Name = ProfilePartName, Type = typeof(Button))]
+    public  class ChattingMessageControl : Control
+    {
+        private const string ProfilePartName = "PART_Profile";
+        private Button? _profilePart;
+
+        public ChattingMessageControl()
+        {
+            this.DefaultStyleKey = typeof(ChattingMessageControl);
+        }
+
+        public static readonly DependencyProperty ChatContentProperty =
+            DependencyProperty.Register("ChatContent", typeof(object), typeof(ChattingMessageControl)
+                , new UIPropertyMetadata(null));
+
+        public object? ChatContent
+        {
+            get { return base.GetValue(ChatContentProperty) as object; }
+            set { base.SetValue(ChatContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty? SelectionBackgroundProperty =
+            DependencyProperty.Register(
+                "SelectionBackground",
+                typeof(Brush),
+                typeof(ChattingMessageControl),
+                new PropertyMetadata((SolidColorBrush)(new BrushConverter().ConvertFrom("#FF606264"))!));
+
+        public Brush SelectionBackground
+        {
+            get { return (Brush)this.GetValue(SelectionBackgroundProperty); }
+            set { this.SetValue(SelectionBackgroundProperty, value); }
+        }
+    }
+}
