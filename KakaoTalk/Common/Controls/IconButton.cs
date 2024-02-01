2b7ac859f75d5d90e261a71e6cb3ec28cde1fbf4
AhnYeonghoo <dksdudgn00@naver.com>
2024-02-01 16:59:35 +0900

    Definition Style

--- KakaoTalk/Common/Controls/IconButton.cs
+++ KakaoTalk/Common/Controls/IconButton.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
-namespace KakaoTalk.Common.Controls
+namespace Common.Controls
 {
-    internal class IconButton
+    public class IconButton : Button
     {
+        public IconButton()
+        {
+            DefaultStyleKey = typeof(IconButton);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        public static readonly DependencyProperty? IconImageProperty =
+            DependencyProperty.Register("IconImage", typeof(ImageSource), typeof(IconButton));
+
+        public ImageSource? IconImage
+        {
+            get { return base.GetValue(IconImageProperty) as ImageSource; }
+            set { base.SetValue(IconImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty? TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(IconButton));
+
+        public String? Text
+        {
+            get
+            {
+                return base.GetValue(TextProperty) as String;
+            }
+            set
+            {
+                base.SetValue(TextProperty, value);
+            }
+        }
     }
 }
